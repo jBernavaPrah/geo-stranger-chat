33d5df80ac81bc877e62f0bdd8d1c6b5aba07f0b
Jure Bernava Prah <webjure@gmail.com>
2018-01-27 19:19:40 +0100

    some improve

--- UniversalBot/SkypeBot/__init__.py
+++ UniversalBot/SkypeBot/__init__.py
@@ -1,6 +1,8 @@
 import urlparse
 
+import logging
 import requests
+import time
 
 
 class SkypeSendException(Exception):
@@ -8,8 +10,33 @@ class SkypeSendException(Exception):
 
 
 class SkypeBot(object):
-	def __init__(self, key):
+	def __init__(self, client_id, key):
 		self.skype_key = key
+		self.client_id = client_id
+		self._next_token = None
+		self._token = None
+
+	def _retrieve_token(self):
+		try:
+			payload = "grant_type=client_credentials&client_id=" + self.client_id + "&client_secret=" + self.skype_key + "&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default"
+			response = requests.post(
+				"https://login.microsoftonline.com/common/oauth2/v2.0/token?client_id=" + self.client_id + "&client_secret=" + self.skype_key + "&grant_type=client_credentials&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default",
+				data=payload, headers={"Content-Type": "application/x-www-form-urlencoded"})
+			data = response.json()
+			self._token = data["access_token"]
+			self._next_token = time.time() + 3300
+
+		except Exception as e:
+			logging.exception(e)
+			time.sleep(0.7)
+			self._retrieve_token()
+
+	@property
+	def token(self):
+		if not self._token or not self._next_token or self._next_token < time.time():
+			self._retrieve_token()
+
+		return self._token
 
 	def _send_request(self, service, conversation_id, payload):
 		try:
@@ -17,8 +44,9 @@ class SkypeBot(object):
 			_url = urlparse.urljoin(service, '/v3/conversations/' + conversation_id + '/activities/')
 
 			result = requests.post(_url,
-								   headers={"Authorization": "Bearer " + self.skype_key, "Content-Type": "application/json"},
-								   json=payload)
+			                       headers={"Authorization": "Bearer " + self.token,
+			                                "Content-Type": "application/json"},
+			                       json=payload)
 
 			return result.status_code
 		except Exception as e:
