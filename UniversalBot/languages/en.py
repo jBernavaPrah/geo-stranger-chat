f3a1a41e936e317d46395da3afdaabb2116e2387
Jure Bernava Prah <webjure@gmail.com>
2018-01-30 17:47:14 +0100

    Aggiunto webchat!!!! :DDDDDD

--- UniversalBot/languages/en.py
+++ UniversalBot/languages/en.py
@@ -10,7 +10,7 @@ terms = u'Find our terms here: https://geostranger.com/terms'
 _terms = u"By using the GeoStranger bot, and/or related products and/or services (\"GeoStranger\", provided by GeoStranger.com), you agree to the following terms:\n\nDo not use GeoStranger if you are under 13. If you are under 18, use it only with a parent/guardian's permission. Do not transmit nudity, sexually harass anyone, publicize other peoples' private information, make statements that defame or libel anyone, violate intellectual property rights, use automated programs to start chats, or behave in any other inappropriate or illegal way on GeoStranger. Understand that human behavior is fundamentally uncontrollable, that the people you encounter on GeoStranger may not behave appropriately, and that they are solely responsible for their own behavior. Use GeoStranger at your own peril. Disconnect if anyone makes you feel uncomfortable. You may be denied access to GeoStranger for inappropriate behavior, or for any other reason. GEOSTRANGER IS PROVIDED AS IS, AND TO THE MAXIMUM EXTENT ALLOWED BY APPLICABLE LAW, IT IS PROVIDED WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED, NOT EVEN A WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. TO THE MAXIMUM EXTENT ALLOWED BY APPLICABLE LAW, THE PROVIDER OF GEOSTRANGER, AND ANY OTHER PERSON OR ENTITY ASSOCIATED WITH GEOSTRANGER'S OPERATION, SHALL NOT BE HELD LIABLE FOR ANY DIRECT OR INDIRECT DAMAGES ARISING FROM THE USE OF GEOSTRANGER, OR ANY OTHER DAMAGES RELATED TO GEOSTRANGER OF ANY KIND WHATSOEVER. By using GeoStranger, you accept the practices outlined in GeoStranger's PRIVACY POLICY.",
 privacy = u''
 user_required_but_not_found = u"Hmm how are you?? haha I don't know you, but this is not important, we can start now! :) ",
-help = u'*List of command you can use with me:*\n\n{help_text}',
+help = u'Hi GeoStranger! My work is to find new friend near you!\n\nOnce you have completed the initial phase you can search for a new GeoStranger by sending command */search*. If you send me */search* again, during the chat, I will look for you to find a new GeoStranger.\nTo not receive other GeoStranger send me the */stop* command. *List of command you can use with me:*\n\n{help_text}',
 
 ask_stop_sure = u'Are you sure to stop receiving new GeoStranger?'
 ask_stop_also_current_chat = u'Are you sure to stop receiving new GeoStranger?\n\nThis will stop also current chat.'
@@ -25,16 +25,16 @@ ask_delete_sure = u"*Are you sure to delete all your data and stop talk with oth
 not_deleted = u"Good, *I haven't delete anythings*",
 delete_completed = u"I have deleted all association of you in our data. Remember to delete also this chat.\n\n *Bye bye*. To restart, send me a message or use command /start.",
 
-welcome = u"*Welcome GeoStranger!* \U0001F600\U0001F600!\n\nIf you continue and answare my asks, you accept the /terms of my creators.\nYou can delete all your information from my databases with command /delete.\n\n For the complete list of commands use /help.",
+welcome = u"*Welcome GeoStranger!* \U0001F600\U0001F600!\n\nIf you continue and answare my questions, you accept the terms of my creators (send me /terms to see its.).\nYou can delete all your information from my databases with command */delete*.\n\nFor the complete list of commands use */help*. \n\nTo send command simply click on button if present or send relative command with \"/\" in front. Example: \"/location\" ",
 
-ask_location = u'From where you write me?\n\n Enter the name of City or send me your location (if your device have that function)',
-re_ask_location = u'Where are you?',
-location_not_found = u"I haven't found {location_text}. Retry with other name send me your location (if your device have that function)..",
-location_error = u"I cannot continue if you not send me your position. What is your location?",
+ask_location = u'To found best GeoStranger that match to you I need only one information, your current location. From where you write me?\n\n Enter the name of City and Region. *Nobody will know your real location.* GeoStranger will only see the name of your town/city.',
+re_ask_location = u'Then, what is your current location?\n\n',
+location_not_found = u"I haven't found {location_text}. Retry with other city or be more specific..",
+location_error = u"I cannot continue if you not send me your position. What is your current location?",
 ask_location_is_correct = u"Is correct this position?\n\n {location_text}",
-location_saved = u'Ok, I have saved *{location_text}* location.\n\nTo change it, in future, use command /location.',
+location_saved = u'Ok, I have saved *{location_text}* location.\n\nTo change it, use command /location.',
 
-search = u'To start new chat use command /search.\n\nList of all commands /help.'
+search = u'To start new chat send me command /search.\n\nList of all commands /help.'
 in_search = u'I have start searching..',
 
 ask_sex = u'What your gender?',
@@ -47,11 +47,12 @@ age_error = u"Sorry, I'm not understand.. How old are you?",
 age_error_to_low = u"Are *parents* with you? My creators are not happy if you are alone. ",
 age_error_to_high = u"Sure you have {age}?? \n\nI'll save, but you can change later if is worng... ;) ",
 
-found_new_geostranger_first_time = u'Super! I have found your GeoStranger, near *{location_text}*. Now, all message, video, image or audio will be sent directly to this GeoStranger in anonymously mode.',
-found_new_geostranger = u'New GeoStranger, near *{location_text}*.'
+found_new_geostranger_first_time = u'Super! I have found your GeoStranger, near *{location_text}*. Now, all message, video, image or audio will be sent directly to this GeoStranger in anonymously mode. (Use /stop to stop receiving new GeoStrangers)',
+found_new_geostranger = u'New GeoStranger, near *{location_text}*. Start chat with it. (Use /stop to stop receiving new GeoStrangers)'
 
-ask_notify = u'Ok, now write your notifycation :) I will send it to our developers!'
-notify_sent = u'Notify sended!'
+ask_notify = u'Ok, now write your notification. :) I will send it to our developers!'
+notify_sent = u'Notify sent!'
+
 go_to_real_geostranger_account = u'Hi. This is only an alias. Use @GeoStrangerBot instead. Thanks.'
 
 completed = u'Ok! :) We have finish. \n\n Now use /search to start talk and found new friends!',
@@ -64,4 +65,4 @@ play_audio = u'GeoStranger have sent an audio. Click link to play it:\n\n{url}'
 play_video = u'GeoStranger have sent a video. Click link to play it:\n\n{url}'
 download_file = u'GeoStranger have sent a document. Click link to download it:\n\n{url}'
 
-not_compatible = u'This message is not compatible. It will not be sent to GeoStranger.'
+not_compatible = u'I cannot understand that message sorry :(  It will not be sent to GeoStranger.'
